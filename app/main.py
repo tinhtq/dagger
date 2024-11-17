e1ec9a2e62e61474d1eb11539394ae45978962f7
tinhtq <truongquangtinh1997@gmail.com>
2024-11-17 18:40:42 +0700

    test error

--- app/main.py
+++ app/main.py
@@ -10,6 +10,10 @@ def read_root():
     return {"message": "Hello, FastAPI"}
 
 
+xyz
+
+
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: str = None):
+    os.get("env")
     return {"item_id": item_id, "q": q}
